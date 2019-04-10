7142ef40376d91a2a10aadce8453986cd96dbf3a
owensengoku <owensengoku@gmail.com>
2019-04-10 23:54:40 +0800

    add restart prometheus script ;

---
+++ CH_2/coredns_scrape_configs/restart_prometheus.sh
@@ -0,0 +1,2 @@
+kubectl patch statefulset prometheus-k8s -n monitoring -p '{"spec":{"template":{"metadata":{"annotations":{"last-manual-restart":"'$(date +%F@%R:%S)'"}}}}}' 
+kubectl get po prometheus-k8s-0 -n monitoring
