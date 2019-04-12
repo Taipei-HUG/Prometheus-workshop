dfcbdf58ddc57edd8dabc45a654d704b1a396a0d
owensengoku <owensengoku@gmail.com>
2019-04-12 14:19:50 +0800

    CH_2: update script for reload prometheus

--- CH_2/coredns_service_monitor/restart_prometheus.sh
+++ CH_2/coredns_service_monitor/restart_prometheus.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 kubectl patch statefulset prometheus-k8s -n monitoring -p '{"spec":{"template":{"metadata":{"annotations":{"last-manual-restart":"'$(date +%F@%R:%S)'"}}}}}' 
 kubectl get po prometheus-k8s-0 -n monitoring
