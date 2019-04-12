ccf67984a84d311f5eca6704a872671f36cbe340
adam <adam.cws@gmail.com>
2019-04-12 18:32:23 +0800

    add missing arg

--- CH_4/apply_change.sh
+++ CH_4/apply_change.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 kubectl -n monitoring delete secret alertmanager-main
 sleep 10
-kubectl -n monitoring create secret alertmanager-main --from-file=alertmanager.yaml
+kubectl -n monitoring create secret generic alertmanager-main --from-file=alertmanager.yaml
