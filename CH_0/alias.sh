4f6d6483391ed4bf7bb84eabf900129d20e2720b
adam <adam.cws@gmail.com>
2019-04-13 01:13:55 +0800

    add alias script can type less words

---
+++ CH_0/alias.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+alias k='kubectl'
+alias kd='kubectl delete'
+alias ka='kubectl apply'
+alias km='kubectl -n monitoring'
+alias kma='kubectl -n monitoring apply'
+alias kmd='kubectl -n monitoring delete'
