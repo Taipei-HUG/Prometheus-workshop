f1a373b31bff9e9b853e270a27737a916a1f502a
adam <adam.cws@gmail.com>
2019-04-12 18:05:51 +0800

    add alertmanager ELB

--- CH_0/get_links.sh
+++ CH_0/get_links.sh
@@ -8,3 +8,6 @@ echo "http://"$(kubectl get svc prometheus-k8s -n monitoring -o jsonpath="{.stat
 
 echo "Pushgateway"
 echo "http://"$(kubectl get svc my-pushgateway -n monitoring -o jsonpath="{.status.loadBalancer.ingress[0].hostname}")":9091"
+
+echo "Alertmanager"
+echo "http://"$(kubectl get svc alertmanager-main -n monitoring -o jsonpath="{.status.loadBalancer.ingress[0].hostname}")":9093"
